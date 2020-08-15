3a5d3d25b299e7a5a5f95937377b2a728d68543e
mahmoud 32salamn <mahmoud32salamn@gmail.com>
2020-08-15 23:32:59 +0200

    modify some props

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -2,6 +2,8 @@ import React, { useState, useContext } from "react";
 import { SignUpContext } from "./SignUpContext";
 import axios from "axios";
 import { Button } from "react-bootstrap";
+import { Redirect } from "react-router-dom";
+
 const Booking = (props) => {
   const [phone, setPhone] = useState("");
   const [personsNum, setPersonsNum] = useState("");
@@ -33,52 +35,49 @@ const Booking = (props) => {
     setPhone("");
     setPersonsNum(0);
     setTravelDegree(0);
+
+   props.history.push('/profile')
   };
   return (
     <div className="bookingBackground ">
-        
-     <div className="form-wrapper">
-     <h2>إحجز الآن</h2>
-      <form onSubmit={handelSubmit}>
-     
-        <div>
-          <label>ادخل رقم التليفون </label>
-          <input
-            name="phone"
-            type="text"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>ادخل عدد الافراد</label>
-          <input
-            name="personsNum"
-            type="number"
-            value={personsNum}
-            onChange={(e) => setPersonsNum(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>ادخل درجة السفر</label>
-          <input
-            name="travelDegree"
-            type="number"
-            value={travelDegree}
-            onChange={(e) => setTravelDegree(e.target.value)}
-          />
-        </div>
+      <div className="form-wrapper">
+        <h2>إحجز الآن</h2>
+        <form onSubmit={handelSubmit}>
+          <div>
+            <label>ادخل رقم التليفون </label>
+            <input
+              name="phone"
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>ادخل عدد الافراد</label>
+            <input
+              name="personsNum"
+              type="number"
+              value={personsNum}
+              onChange={(e) => setPersonsNum(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>ادخل درجة السفر</label>
+            <input
+              name="travelDegree"
+              type="number"
+              value={travelDegree}
+              onChange={(e) => setTravelDegree(e.target.value)}
+            />
+          </div>
 
-        <div className="submit">
-              <Button type="submit" variant="warning ">
-                إحجز
-              </Button>
-            </div>
-      </form>
-     
+          <div className="submit">
+            <Button type="submit" variant="warning ">
+              إحجز
+            </Button>
+          </div>
+        </form>
       </div>
-          
-    
     </div>
   );
 };
